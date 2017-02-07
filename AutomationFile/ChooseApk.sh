31f3a82c42483bfe0b89356e1377e66555b426ba
zhaojiabi <747379059@qq.com>
2017-02-07 20:30:40 +0800

    2.7update

--- AutomationFile/ChooseApk.sh
+++ AutomationFile/ChooseApk.sh
@@ -13,45 +13,61 @@ echo $date_time
 #根据获取的星期判断具体执行的apk文件
 case $date_time in
 	一 )
-		echo today is Monday;;
+		echo today is Monday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test1";;
 	二 )
 		echo today is Tuseday
 		ApkPath="./apk_file/ticktask.apk"
-		Tag="Test";;
+		Tag="Test2";;
 	三 )
 		echo today is Wednesday
 		ApkPath="./apk_file/ticktask.apk"
-		Tag="UC001";;
+		Tag="Test3";;
 	四 )
-		echo today is Thurday;;
+		echo today is Thurday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test4";;
 	五 )
 		echo today is Firday
 		ApkPath="./apk_file/ticktask.apk"
-		Tag="UC001";;
+		Tag="Test5";;
 	六 )
-		echo today is Saturday;;
+		echo today is Saturday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test6";;
 	日 )
-		echo today is Sunday;;
+		echo today is Sunday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test7";;
 	Mon )
-		echo today is Monday;;
+		echo today is Monday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test1";;
 	Tue )
 		echo today is Tuseday
 		ApkPath="./apk_file/ticktask.apk"
-		Tag="Test";;
+		Tag="Test2";;
 	Wed )
 		echo today is Wednesday
 		ApkPath="./apk_file/ticktask.apk"
-		Tag="UC001";;
+		Tag="Test3";;
 	Thu )
-		echo today is Thurday;;
+		echo today is Thurday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test4";;
 	Fri )
 		echo today is Firday
 		ApkPath="./apk_file/ticktask.apk"
-		Tag="UC001";;
+		Tag="Test5";;
 	Sat )
-		echo today is Saturday;;
+		echo today is Saturday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test6";;
 	Sun )
-		echo today is Sunday;;
+		echo today is Sunday
+		ApkPath="./apk_file/ticktask.apk"
+		Tag="Test7";;
 esac
 
 #获取到传递的参数
--- AutomationFile/features/step_definitions/test_feature/first_feature.feature
+++ AutomationFile/features/step_definitions/test_feature/first_feature.feature
@@ -1,6 +1,6 @@
 Feature: Install apk file
 
-  @Test
+  @Test1 @Test2 @Test3 @Test4 @Test5 @Test6 @Test7 
   Scenario: Install apk file successful
 
   	Given I see the guide page
